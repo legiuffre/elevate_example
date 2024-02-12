2356fad9efd873f16baed9339bb54698274250c8
legiuffre <134074791+legiuffre@users.noreply.github.com>
2024-02-12 13:13:01 -0600

    Update usage-api.js
    logged out the json response

--- api/usage_history/usage-api.js
+++ api/usage_history/usage-api.js
@@ -13,6 +13,6 @@ function usageHistoryGroup(dateFrom, dateTo, sortColumn, descending, accountId, 
     if(accountId) searchParams.append("accountId", accountId);
 
     url += `?${searchParams.toString()}`;
-    core.setOutput("json",makeRequest("POST", url, body).then( response => response.json());
+    console.log(response.json());
     return makeRequest("POST", url, body).then( response => response.json());
 }
