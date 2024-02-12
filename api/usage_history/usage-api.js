5b38293d8b24e74c75bfde5d71ad9bb568d19145
legiuffre <134074791+legiuffre@users.noreply.github.com>
2024-02-12 12:45:45 -0600

    Update usage-api.js
    test json data again with output

--- api/usage_history/usage-api.js
+++ api/usage_history/usage-api.js
@@ -13,6 +13,6 @@ function usageHistoryGroup(dateFrom, dateTo, sortColumn, descending, accountId, 
     if(accountId) searchParams.append("accountId", accountId);
 
     url += `?${searchParams.toString()}`;
-
-    return response.json();
+    core.setOutput("json",response.json());
+    return makeRequest("POST", url, body).then( response => response.json());
 }
